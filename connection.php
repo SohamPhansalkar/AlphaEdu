5186b53fd28ef7917356b8edf04980aae7b8c838
Soham P Phasalkar <sohampp26nov@gmail.com>
2022-11-16 19:19:49 +0530

    Removal of passwords
    Important passwords have been replaced with the fakes ones

--- connection.php
+++ connection.php
@@ -2,14 +2,14 @@
 
     // INSERT INTO `vidyalankar_edu` (`name`, `password`, `other`) VALUES ('Keshav Phansalkar', '1234', 'for future');
 
-    $serevr = "localhost";
-    $username = "id19449432_root";
-    $password = "Gampuzampu@1111";
+    $serevr = "TheAlphaEdu";
+    $username = "id234567876546354475624564786536475_root";
+    $password = "Alpha******@root";
     $dbname = "id19449432_vidyalankar_edu";
 
     //$conn = new mysqli($serevr, $username, $password, $dbname);
     //if(!$nonn){
         //die(mysqli_error($conn));
-    //}
+    //} All the imp information has been modified
 
 ?>
